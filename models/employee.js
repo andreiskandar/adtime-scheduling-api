3bdc6a53a05a37036e79b5991e3b20cc8901f33e
Thai Do <thai.hl.do@gmail.com>
2020-10-21 21:13:10 -0700

    Trying to setup auth routes to connect from FEto BE

--- models/employee.js
+++ models/employee.js
@@ -1,7 +1,11 @@
 module.exports = function Employee({
   firstName = '',
   lastName = '',
+  email = '',
+  password = '',
 }) {
   this.firstName = firstName
   this.lastName = lastName
+  this.email = email
+  this.password = password
 }
--- routes/login.js
+++ routes/login.js
@@ -3,19 +3,13 @@ const manager = require('./manager');
 const router = express.Router();
 const managers = require('./manager')
 
-// Test code
-let users = [
-  {
-    email: 'test@test.com', password: 'password'
-  }
-]
 
 module.exports = router
   .get('/', (req, res) => {
     res.status(200).json({hello: 'world'})
   })
 
-  .post('/login', (req,res) => {
+  .post('/', (req,res) => {
     let result = managers.find(manager => manager.email == req.body.email);
     if(result) {
       if(result.password == req.body.password) {
