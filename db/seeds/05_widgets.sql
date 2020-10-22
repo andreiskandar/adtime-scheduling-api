f5ee0aa4c8efbea4cac56cf90c20aba83d701ce2
David Ramalho <david.m.ramalho@gmail.com>
2020-10-22 04:46:40 +0000

    db / db:reset

---
+++ db/seeds/05_widgets.sql
@@ -0,0 +1,5 @@
+-- Widgets table seeds here (Example)
+INSERT INTO shifts (name, user_id) VALUES ('Shift 1', 1);
+INSERT INTO shifts (name, user_id) VALUES ('Shift 2', 2);
+INSERT INTO shifts (name, user_id) VALUES ('DUMBShift 3', 2);
+INSERT INTO shifts (name, user_id) VALUES ('DUMBShift over9000', 2);
