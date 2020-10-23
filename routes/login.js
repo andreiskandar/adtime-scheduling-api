356d3e73a34208d55048faf7f508f7b2e9fef8ab
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-23 10:42:05 -0700

    before merge to fix cors

--- routes/login.js
+++ routes/login.js
@@ -7,25 +7,8 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  console.log('WTF ARE YOU DOING?', req.body);
+  console.log('login routes');
   res.json({ message: 'message' });
 });
-// let result = manager.filter(managers => managers.email === req.body.email);
 
-// console.log("WHAT ARE YOU", result)
-// if(result) {
-//   if(result[0].password === req.body.password) {
-//     res.status(200).send({
-//       message: "Successful login!"
-//     })
-//   } else {
-//     res.status(200).send({
-//       message: "Password Incorrect!"
-//     })
-//   }
-// } else {
-//   res.status(200).send({
-//     message: "User Not found incorrect!"
-//   })
-// }
 module.exports = router;
