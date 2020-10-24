873300c5048fe2e0df6dcd8fc19b9fddab594203
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-24 10:21:00 -0700

    need to change the query for getShiftForUser

--- controllers/events.js
+++ controllers/events.js
@@ -2,13 +2,16 @@ const Model = require('../models/employee');
 const db = require('./db');
 
 //GET all shifts by user
+
 const getShiftsByUser = () => {
+  // should receive user_id, date range
   const queryString = `
-          SELECT *, shifts.hours as hours
+          SELECT users.id as user_id, users.name as name, shifts.hours as hours, events.event_date, shifts.id as shift_id
           FROM events
-          JOIN shifts ON events.shift_id = shifts.id 
+          JOIN shifts ON events.shift_id = shifts.id
+          JOIN users ON events.user_id = users.id
           WHERE user_id = 1 AND
-          event_date >= '2020-10-22' AND event_date <= '2020-10-26';`;
+          event_date >= '2020-10-19' AND event_date <= '2020-10-26';`;
   return db.query(queryString).then((response) => {
     return response.rows;
   });
