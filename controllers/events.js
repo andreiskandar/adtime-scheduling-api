a4ddaf99cbd1ad0289c3a7dbb3e34e1a8b4e016e
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-24 12:50:51 -0700

    created query for adding new shift and transfer shift to user

--- controllers/events.js
+++ controllers/events.js
@@ -11,11 +11,24 @@ const getShiftsByUser = () => {
           JOIN shifts ON events.shift_id = shifts.id
           JOIN users ON events.user_id = users.id
           WHERE user_id = 1 AND
-          event_date >= '2020-10-19' AND event_date <= '2020-10-26';`;
+          event_date >= '2020-10-19' AND event_date <= '2020-10-26'
+          SORT_BY event_date;`;
   return db.query(queryString).then((response) => {
     return response.rows;
   });
 };
+
+//should accept 3 arguments: user_id, shift_id, category_id, date
+const addShiftsByUser = (user_id, shift_id, category_id, date) => {
+  const queryString = `
+    INSERT INTO events (user_id, shift_id, category_id, event_date, isPublished) VALUES ($1::integer, $2::integer, $3::integer, $4::date, f)
+  
+  `;
+};
+
+// react should be able to select multiple grids (find out what event listener to use. onClickHold)
+const updateShiftString = `UPDATE events SET user_id = 2
+WHERE user_id = 3 AND shift_id IN (8,9) AND event_date = '2020-10-27';`;
 
 module.exports = {
   getShiftsByUser,
--- db/seeds/05_events.sql
+++ db/seeds/05_events.sql
@@ -32,9 +32,9 @@ INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (2, 6, 1,
 INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (2, 7, 1, '2020-10-26');
 INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (2, 8, 1, '2020-10-26');
 INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (2, 6, 1, '2020-10-24');
-INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (3, 6, 1, '2020-10-24');
+INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (3, 5, 2, '2020-10-24');
 INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (3, 6, 1, '2020-10-27');
-INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (3, 6, 1, '2020-10-27');
-INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (3, 6, 1, '2020-10-27');
-INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (3, 6, 1, '2020-10-27');
+INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (3, 7, 1, '2020-10-27');
+INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (3, 8, 1, '2020-10-27');
+INSERT INTO events (user_id, shift_id, category_id, event_date) VALUES (3, 9, 1, '2020-10-27');
 
