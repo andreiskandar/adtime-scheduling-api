308dbd08965a06f08f3de28d20032805bb61aea2
David Ramalho <david.m.ramalho@gmail.com>
2020-10-25 00:55:10 +0000

    added query loop for adding events

--- controllers/events.js
+++ controllers/events.js
@@ -19,30 +19,27 @@ const getShiftsByUser = () => {
 };
 
 //should accept 3 arguments: user_id, shift_id, category_id, date
-const addShiftsByUser = (user_id, shift_id, category_id, date) => {
-  // // shift_id = [1,2,3,4,5]
-  // let qS = ''
-  // for(let i of shift_id){
-  //   qS +=
-  //     `INSERT INTO events (user_id, shift_id, category_id, event_date, isPublished) VALUES ($1::integer, ${i}, $2::integer, $3::date, f)`;
+  const addShiftsByUser = (user_id, shift_id, category_id, date) => {
+    const addFunction = (queryString, shiftId) => {
+      return db.query(queryString, [user_id, shiftId, category_id, date]).then((response) => {
+        return response.rows;
+      });
+    }
+    for (const shiftId of shift_id) {
+      console.log(shiftId)
+      let queryString =`
+      INSERT INTO events (user_id, shift_id, category_id, event_date) 
+      VALUES ($1::integer, $2::integer, $3::integer, $4::date);`;
+      addFunction(queryString, shiftId)
+    }
+  };
 
-  // }
-  // const qString = qS, [user_id]
-
-  // return db.query(qString).then((response) => {
-  //   return response.rows;
-  // });
-
-  const queryString = qS`
-    INSERT INTO events (user_id, shift_id, category_id, event_date, isPublished) VALUES ($1::integer, $2::integer, $3::integer, $4::date, f)
-  
-  `;
-};
+addShiftsByUser(7, [3, 4, 5, 6, 7], 1, '2020-10-22');
 
 // react should be able to select multiple grids (find out what event listener to use. onClickHold)
 const updateShiftString = `UPDATE events SET user_id = 2
 WHERE user_id = 3 AND shift_id IN (8,9) AND event_date = '2020-10-27';`;
 
 module.exports = {
-  getShiftsByUser,
+  getShiftsByUser, addShiftsByUser
 };
