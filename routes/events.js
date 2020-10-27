47276f097cb8f02692f6a98c723fdda1530c06de
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-27 13:49:31 -0700

    fixed proxy request

--- routes/events.js
+++ routes/events.js
@@ -5,12 +5,12 @@ const { publishWeek, grabShiftId, addShiftsByUser, transferShift } = require('..
 // PUT to update and Transfer Shifts
 router.put('/transfer', async (req, res) => {
   try {
-    const {user_id, shift_id, category_id, event_date, transferToId} = req.body
-    const transfer = await transferShift(user_id, shift_id, transferToId)
-    res.status(200).json(transfer)
-  } catch(err) {
-    console.error("Transfer of Shifts Error: ", err)
-    res.status(400).json({msg: "Cannot complete transfer of shifts from routes"})
+    const { user_id, shift_id, category_id, event_date, transferToId } = req.body;
+    const transfer = await transferShift(user_id, shift_id, transferToId);
+    res.status(200).json(transfer);
+  } catch (err) {
+    console.error('Transfer of Shifts Error: ', err);
+    res.status(400).json({ msg: 'Cannot complete transfer of shifts from routes' });
   }
 });
 
@@ -21,11 +21,12 @@ router.put('/publish', (req, res) => {
 });
 
 // ADD EVENTS
+//api/events/add
 router.post('/add', async (req, res) => {
   try {
     const { user_id, shift_id, category_id, event_date } = req.body;
     await addShiftsByUser(user_id, shift_id, category_id, event_date);
-    res.status(200);
+    res.status(200).send();
   } catch (err) {
     console.error('addEvents ERROR:', err);
     res.status(400).json({ msg: 'Not sure what you are trying to accomplish...' });
@@ -38,7 +39,7 @@ router.delete('/delete', async (req, res) => {
   try {
     const { shift_id, event_date, user_id } = req.query;
     await grabShiftId(shift_id, event_date, user_id);
-    res.status(200);
+    res.status(200).send();
   } catch (err) {
     console.error('RemoveShiftIdRoute:', err);
     res.status(401).json({ msg: 'Invalid Shift IDs' });
