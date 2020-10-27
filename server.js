e34c22e99f73fd441a7ecfc351b64bf9cf0a559b
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-27 15:01:00 -0700

    added node-schedule for cronjob

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const bodyParser = require('body-parser');
 // const sass       = require("node-sass-middleware");
 const routes = require('./routes');
 const morgan = require('morgan');
+const SlackBot = require('slackbots');
+const schedule = require('node-schedule');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -16,6 +19,24 @@ app.use(morgan('dev'));
 for (const [mountPoint, router] of Object.entries(routes)) {
   app.use(mountPoint, router);
 }
+
+const bot = new SlackBot({
+  token: process.env.SLACK_BOT_API_KEY,
+  name: 'MomentBot',
+});
+
+const params = {
+  icon_emoji: 'Bear',
+};
+
+const customMessage = `Hello World from MomentBot ${Date.now()}`;
+
+bot.on('start', () => {
+  schedule.scheduleJob('2 * * * *', () => {
+    bot.postMessage('U01DHGWDFPS', `${customMessage}`, params);
+    // bot.postMessageToChannel('general', `${customMeåssage}`, params);
+  });
+});
 
 const port = process.env.port || 3001;
 app.listen(port, () => {
