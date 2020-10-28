8f1eb19e6f7a55365ba6ff208938336bf3b3376c
Thai Do <thai.hl.do@gmail.com>
2020-10-28 00:12:06 -0700

    Fixing conflicts

--- models/transfershift.js
+++ models/transfershift.js
@@ -1,15 +1,9 @@
 const db = require('../controllers/db');
 
-<<<<<<< HEAD
-function transferShiftId(userId, shiftId, transferToId, category_id) {
-  const transferShift = (queryString, shift_id, transferToId, category_id) => {
-    return  db.query(queryString, [userId, shift_id, transferToId])
-=======
 function transferShiftId(userId, shiftId, transferToId, event_date, category_id) {
   const transferShift = (queryString, shift_id, transferToId, event_date, category_id) => {
     return db
       .query(queryString, [userId, shift_id, transferToId, event_date, category_id])
->>>>>>> master
       .then((response) => {
         // console.log(res.json({response}))
         return true;
@@ -19,16 +13,9 @@ function transferShiftId(userId, shiftId, transferToId, event_date, category_id)
       });
   };
   for (const shift_id of shiftId) {
-<<<<<<< HEAD
-    console.log("SHIFTID IDID I", shift_id)
-
-    const queryString = `UPDATE events SET user_id=$3 WHERE user_id=$1 AND shift_id=$2`;
-    transferShift(queryString, shift_id, transferToId, category_id)    
-=======
     const queryString = `UPDATE events SET user_id=$3 WHERE user_id=$1 AND shift_id=$2 AND event_date=$4 AND category_id=$5`;
     console.log('queryString:', queryString);
     transferShift(queryString, shift_id, transferToId, event_date, category_id);
->>>>>>> master
   }
 }
 
