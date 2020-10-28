4ec03abc8b91be3c0d3042d24acfeabd43f553e3
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-28 13:11:36 -0700

    fixed timezone

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 
 const ENV = process.env.ENV || 'development';
+process.env.TZ = 'UTC';
 const express = require('express');
 const bodyParser = require('body-parser');
 // const sass       = require("node-sass-middleware");
