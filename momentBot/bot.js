11b4fac702bd4d59e624467f27e95a09ad48a639
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-29 11:56:27 -0700

    tested to send reminders to all users

--- momentBot/bot.js
+++ momentBot/bot.js
@@ -10,24 +10,30 @@ const params = {
   icon_emoji: 'robot',
 };
 
+function processDataToPost(apiData) {
+  return apiData.map((data, idx) => {
+    const { name, slack_username, event_name, event_date } = data;
+    const datetime = String(event_date).split(' ');
+    const time = datetime[4].split(':');
+    const date = `${datetime[0]}, ${datetime[1]} ${datetime[2]} ${datetime[3]}`;
+    const appointment_time = `${time[0]}:${time[1]}`;
+    const message = `${idx} - ${name}, you have an ${event_name} at ${appointment_time}`;
+    // bot.postMessageToUser('slack_username', `${message}`, params);
+    return bot.postMessageToUser('andre.m.iskandar', `${message}`, params).then(() => true);
+  });
+}
+
+const minute = 7 / 60;
+
 const sendReminderToUser = () => {
   setInterval(() => {
     getEventsForReminder()
       .then((data) => {
-        if (data) {
-          console.log('data:', data);
-          const { name, slack_username, event_name, event_date } = data;
-          const datetime = String(event_date).split(' ');
-          const time = datetime[4].split(':');
-          const date = `${datetime[0]}, ${datetime[1]} ${datetime[2]} ${datetime[3]}`;
-          const appointment_time = `${time[0]}:${time[1]}`;
-
-          const message = `${name}, you have an ${event_name} at ${appointment_time}`;
-          bot.postMessageToUser('andre.m.iskandar', `${message}`, params);
-        }
+        data && processDataToPost(data);
       })
+      .then(() => bot.postMessageToUser('andre.m.iskandar', `===================`, params))
       .catch((e) => console.log('error', e));
-  }, 60000);
+  }, minute * 60000);
 };
 
 module.exports = { bot, sendReminderToUser };
