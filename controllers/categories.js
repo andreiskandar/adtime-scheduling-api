930e2aef8aac81f7d56c03b6817d11ec8e876d8f
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-30 12:07:18 -0700

    received availability from the front end

--- controllers/categories.js
+++ controllers/categories.js
@@ -6,4 +6,15 @@ const getCategories = () => {
   });
 };
 
-module.exports = { getCategories };
+const updateAvailability = () => {
+  //all the days and start time and end time
+  //structure startTime or EndTime
+  const startTimeId = { Friday: 0, Monday: 0, Saturday: 0, Sunday: 0, Thursday: 0, Tuesday: 0, Wednesday: 0 };
+
+  // create a function to find the first monday, or tuesday
+  // find the next mondays, tuesdays...
+  // insert statement to database
+  //
+};
+
+module.exports = { getCategories, updateAvailability };
--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getCategories } = require('../controllers/categories');
+const { getCategories, updateAvailability } = require('../controllers/categories');
 
 router.get('/', (req, res) => {
   getCategories()
@@ -12,4 +12,13 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/updateAvailability/:user_id', (req, res) => {
+  const { startTimeState, endTimeState } = req.query;
+  const { user_id } = req.params;
+  console.log('endTimeState:', JSON.parse(endTimeState));
+  console.log('startTimeState:', JSON.parse(startTimeState));
+  console.log('user_id:', user_id);
+
+  updateAvailability(user_id, startTimeState, endTimeState).then().catch();
+});
 module.exports = router;
