0a989df844e1db207856cd87b787d7f628a68fe9
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-11-04 23:38:39 -0800

    fixed slackbot and twilio

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const bodyParser = require('body-parser');
 // const sass       = require("node-sass-middleware");
 const routes = require('./routes');
 const morgan = require('morgan');
+const SlackBot = require('slackbots');
 
-const { sendReminderToEmp } = require('./twilio/bot')
+const { sendReminderToEmp } = require('./twilio/bot');
 const { bot, sendReminderToUser } = require('./momentBot/bot');
 
 const app = express();
@@ -24,7 +25,7 @@ for (const [mountPoint, router] of Object.entries(routes)) {
 
 bot.on('start', sendReminderToUser);
 
-sendReminderToEmp()
+sendReminderToEmp();
 
 const port = process.env.port || 3001;
 app.listen(port, () => {
