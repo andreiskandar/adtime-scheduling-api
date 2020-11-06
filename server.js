63f506692efc1e82b808b1389654b1227694d439
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-11-06 11:58:53 -0800

    disable twilio bot

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ for (const [mountPoint, router] of Object.entries(routes)) {
   app.use(mountPoint, router);
 }
 
-bot.on('start', sendReminderToUser);
+// bot.on('start', sendReminderToUser);
 
 // sendReminderToEmp();
 
